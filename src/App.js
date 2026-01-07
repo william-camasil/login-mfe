ca851346a91090ec69a12285bf34b50773683bce
willliamcamasil <williamcamargosilva@gmail.com>
2026-01-07 15:46:23 -0300

    feat: adiciona configurações iniciais do projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="login">
+      <p>Login - MFE</p>
+    </div>
+  );
+};
+
+export default App;
